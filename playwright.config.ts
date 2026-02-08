32717e1289710cdd83a6ed9188062fb3b592d16c
NareshMgr <krishnaqa.test5@gmail.com>
2026-02-08 21:12:14 +0530

    added playwright run workflow yml file and updated start server false

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
       embedAttachments: true,
       outputFolder: 'playwright-html-report',
       minifyAssets: true,
-      startServer: true,
+      startServer: false,
     }]
   ],
   
